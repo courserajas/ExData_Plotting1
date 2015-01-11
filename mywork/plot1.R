cb7402d65f74db79feda03c186aeab09bccec7d1
courserajas <courserajas@gmail.com>
2015-01-11 20:03:00 +0200

    typo correction

--- mywork/plot1.R
+++ mywork/plot1.R
@@ -4,7 +4,7 @@ data <- read.csv("household_power_consumption.txt", sep = ";")
 # get "Global Active Power" data of required days
 d <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007", 3]
 
-# open device to create png file. transparent background as in examles in github
+# open device to create png file. transparent background as in examples in github
 png(filename = "plot1.png", width = 480, height = 480, bg = "transparent")
 
 # create the histogram with appropriate style
